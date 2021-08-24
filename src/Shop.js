d8ce06e8d26183f44be6fe81ceef2132cc85958f
reed756 <68922323+reed756@users.noreply.github.com>
2021-08-24 13:28:35 +0100

    added api request and use Effect function to begin to update state for items to poppulate shop page

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,25 @@
 import Navbar from "./components/Navbar";
 import CartDisplay from "./components/CartDisplay";
+import React, {useState, useEffect} from "react";
 
 const Shop = () => {
+
+    useEffect(() => {
+      fetchItems();
+    }, []);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItems = async () => {
+      const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+      // console.log(data);
+
+      const items = await data.json();
+      console.log(items);
+      setItems(items.data);
+
+    }
+
     return (
       <div>
         <Navbar 
@@ -12,6 +30,11 @@ const Shop = () => {
         />
         <CartDisplay />
         <h1>Welcome to the shop!</h1>
+        <div>
+          {items.map(item => {
+            <h3>{item.name}</h3>
+          })}
+        </div>
       </div>
     );
 };
