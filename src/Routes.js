6a2d161a1c31b7947a148a156ef8c39daf9cb252
reed756 <68922323+reed756@users.noreply.github.com>
2021-09-06 10:54:45 +0100

    updated routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,7 @@ import Shop from "./Shop";
 
 const Routes = () => {
   return (
-    <Router basename={"/" || ""}>
+    <Router>
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/shop" component={Shop} />
