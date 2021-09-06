38346beb30d8d4b8f5b108ec8c8273aa6e240f08
reed756 <68922323+reed756@users.noreply.github.com>
2021-09-06 10:39:13 +0100

    updated routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Shop from "./Shop";
 
 const Routes = () => {
   return (
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <Router>
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/shop" component={Shop} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 };
 
